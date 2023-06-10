010caa0969a01dfd2d1f0f76ec0d65b65159bab7
Joy Ahmed <dev.joyahmed@gmail.com>
2023-06-10 12:32:57 +0600

    Added chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+  return <Button colorScheme='blue'>Button</Button>
 }
 
 export default App
